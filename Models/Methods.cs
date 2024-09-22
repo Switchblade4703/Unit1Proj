a82e4647d8dd9c1d786c82317c26eb8d5e9fdd92
Switchblade4703 <156360989+Switchblade4703@users.noreply.github.com>
2024-09-22 09:54:23 -0500

    Add project files.

---
+++ Models/Methods.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Unit1Proj.Models
+{
+    public class Methods
+    {
+        [Required]
+        public string Fighter { get; set; } = "Sonic";
+    }
+}
